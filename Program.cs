1276a20fb32268cc00e2ea5440b296644d42778f
Fishman2703 <90067587+Fishman2703@users.noreply.github.com>
2021-11-06 10:58:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,36 +16,70 @@ int index = 0;
 
 while (index < 10)
 {
-    A[index] = new Random().Next(10, 100);
-    index++;
+    A[index] = new Random().Next(10, 100);
+    index++;
 }
 
 index = 0;
+Console.WriteLine("Массив А");
 
 while (index < 10)
 {
-    Console.WriteLine(A[index]);
-    index++;
+    Console.Write(A[index] + " ");
+    index++;
 }
 
 Console.WriteLine();
 index = 1;
 // Искл. элементов нарушения возрастания
 int currentElement = A[0];
-Console.WriteLine(currentElement);
+Console.WriteLine("нарушители");
+Console.Write(currentElement + " ");
 while(index<10)
 {
-    if(A[index]>currentElement)
-    {
-    Console.WriteLine(A[index]);
-    currentElement = A[index];
-    }
-    index++;
+    if(A[index]>currentElement)
+    {
+    Console.Write(A[index] + " ");
+    currentElement = A[index];
+    }
+    index++;
 } 
-
-Console.WriteLine();
+Console.WriteLine( );
 index = 0;
 // Искл те, которые больше сред ариф массива А
-int summ = 0;
 
+int temp = 0;
+while (index<10)
+{
+      temp = A[index] + temp;
+      index++;
+}
+int midA = temp / 10;
+Console.WriteLine($"среднее арифметическое А = {midA}");
+// Искл.
+index = 0;
+int currentMidA = midA;
+while(index<10)
+{
+    if(A[index]>currentMidA)
+    {
+        Console.Write(A[index]+" ");
+        currentMidA = A[index];
+    }
+    index++;
+}
+//четные
+Console.WriteLine();
+index = 0;
+Console.WriteLine($"четные числа массива А ");
+while(index<10)
+{
+    if(A[index] % 2 ==0)
+    {
+        Console.Write (A[index] +" ");
+    }
+    index++;
+}
+Console.WriteLine(); 
 
+// Вот как сделать массив B из массива A, я не понял((((
